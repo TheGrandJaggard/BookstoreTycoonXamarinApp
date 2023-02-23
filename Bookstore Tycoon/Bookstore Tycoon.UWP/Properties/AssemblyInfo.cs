7846ffd36806544f4b3e1f3d8c9d62d0b9458145
Jaggard <nathancoleman2006@gmail.com>
2023-02-23 15:24:17 -0800

    I have published the first version, changed the icon, and figured out many of the properties for the builds as well as setting them up for v. 1.2

--- Bookstore Tycoon/Bookstore Tycoon.UWP/Properties/AssemblyInfo.cs
+++ Bookstore Tycoon/Bookstore Tycoon.UWP/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bookstore_Tycoon.UWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bookstore Tycoon")]
+[assembly: AssemblyDescription("Bookstore Tycoon for Windows")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Bookstore_Tycoon.UWP")]
@@ -24,6 +25,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-CA")]
